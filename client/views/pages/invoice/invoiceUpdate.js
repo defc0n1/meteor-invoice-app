9aa2ca54f1d1c879f64050a84a5936f501c598e4
Bach Ly <bachchienly@gmail.com>
2015-12-27 22:15:40 +1100

    Calculate Line item total

--- client/views/pages/invoice/invoiceUpdate.js
+++ client/views/pages/invoice/invoiceUpdate.js
@@ -15,5 +15,20 @@ Template.invoiceUpdate.events({
   },
   "click a.delete": function(event, template) {
     Session.set('isUpdatingInvoice', false);
-  }
+  },
+  "blur input.quantity": calculateLineItemTotal,
+  "blur input.unit-price": calculateLineItemTotal,
 })
+
+function calculateLineItemTotal(event, template) {
+  var element = $(event.target),
+      lineItemRow = element.closest('.line-item'),
+      quantityInput = lineItemRow.find('input.quantity'),
+      unitPriceInput = lineItemRow.find('input.unit-price'),
+      totalPriceInput = lineItemRow.find('input.total-price'),
+      quantity = (quantityInput.val() == "" || quantityInput.val() < 0) ? 0 : Number.parseInt(quantityInput.val()),
+      unitPrice = (unitPriceInput.val() == "" || unitPriceInput.val() < 0) ? 0 : Number.parseFloat(unitPriceInput.val()),
+      totalPrice = (quantity * unitPrice).toFixed(2);
+
+  totalPriceInput.val(totalPrice);
+}
