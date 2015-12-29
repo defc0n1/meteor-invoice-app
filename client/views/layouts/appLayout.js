cd10312921d36ebdd5b535a64350b0af5bc4654a
Bach Ly <bachchienly@gmail.com>
2015-12-29 14:25:56 +1100

    Simplify form update/insert/view

--- client/views/layouts/appLayout.js
+++ client/views/layouts/appLayout.js
@@ -1,10 +1,40 @@
-var SELECTED_LIST_ITEM_ID = "selectedListItemID";
+var SELECTED_LIST_ITEM_ID = "selectedListItemID",
+    FORM_ACTION = "currentFormAction",
+    COLLECTION_NAME = "nameOfCurrentCollectionForCRUD";
 
+// Events
 Template.appLayout.events({
   'click #TheList li': function(event, template) {
       Session.set(SELECTED_LIST_ITEM_ID, this._id);
+  },
+  'click a.insert': function(event, template) {
+      Session.set(FORM_ACTION, 'INSERT');
+  },
+  'click a.update': function(event, template) {
+      Session.set(FORM_ACTION, 'UPDATE');
+  }
+});
+
+// Helpers
+Template.registerHelper('isInserting', function() {
+  return (Session.get(FORM_ACTION) == 'INSERT');
+});
+Template.registerHelper('isUpdating', function() {
+  return (Session.get(FORM_ACTION) == 'UPDATE');
+});
+Template.registerHelper('whatTemplate', function() {
+  var controller = Iron.controller();
+  var collection_name =  controller.state.get('COLLECTION_NAME');
+
+  if (Session.get(FORM_ACTION) == 'INSERT') {
+    return collection_name + ".insert";
+  } else if (Session.get(FORM_ACTION) == 'UPDATE') {
+    return collection_name + ".update";
+  } else {
+    return collection_name + ".view";
   }
 })
+
 
 
 /*if (Meteor.isClient) {
