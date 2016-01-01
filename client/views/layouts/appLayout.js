f9e199ff24b4954571810fabbe468c2e24fc8ba9
Bach Ly <bachchienly@gmail.com>
2016-01-01 15:45:32 +1100

    Logout menu and action

--- client/views/layouts/appLayout.js
+++ client/views/layouts/appLayout.js
@@ -1,7 +1,33 @@
+var AUTH_ERRORS_KEY = 'authenticationErrorsKey';
+
 Template.appLayout.helpers({
     addNewUrl: function() {
         var controller = Iron.controller(),
             url = controller.state.get('ADD_NEW_URL');
         return (url) ? url : false;
+    },
+    settingsMenuStatus: function() {
+        return Session.get('SETTINGS_MENU_STATUS');
+    }
+});
+
+Template.appLayout.events({
+    'click a.settingsMenu': function(event, template) {
+        if (Session.get('SETTINGS_MENU_STATUS') == '' || _.isUndefined(Session.get('SETTINGS_MENU_STATUS'))) {
+            Session.set('SETTINGS_MENU_STATUS', 'opened');
+        } else {
+            Session.set('SETTINGS_MENU_STATUS', '');
+        }
+    },
+    'click a.logout': function(event, template) {
+        event.preventDefault();
+
+        Meteor.logout(function(error) {
+            if (error) {
+                return Session.set(AUTH_ERRORS_KEY, {'none': error.reason});
+            }
+
+            Router.go('login');
+        });
     }
 });
