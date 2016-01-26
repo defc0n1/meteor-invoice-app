17ad33cc31d3872e91a5fa8212b8ef35a34c2185
Bach Ly <bachchienly@gmail.com>
2016-01-26 17:50:55 +1100

    Add other invoices fields

--- client/views/modules/invoices/invoicesPrint/invoicesPrint.js
+++ client/views/modules/invoices/invoicesPrint/invoicesPrint.js
@@ -1,21 +1,83 @@
 Template.invoicesPrint.onRendered(function() {
-  var
-    printTemplate = this.data.item.printTemplate,
-    page = {
-      marginTop: printTemplate.pageMarginTop,
-      marginRight: printTemplate.pageMarginTop,
-      marginBottom: printTemplate.pageMarginTop,
-      marginLeft: printTemplate.pageMarginTop
-    };
+    var printTemplate = this.data.item.printTemplate,
+        page = {
+            marginTop: printTemplate.pageMarginTop,
+            marginRight: printTemplate.pageMarginRight,
+            marginBottom: printTemplate.pageMarginBottom,
+            marginLeft: printTemplate.pageMarginLeft
+        };
 
-  $.each(this.data.item.printTemplate.fields, function(index, field) {
-    $("#" + field.name + " .key").css({
-      top: field.labelTop + page.marginTop,
-      left: field.labelLeft + page.marginLeft
+    // page
+    $('#Page').css({
+        width: printTemplate.pageSizeX + 'mm',
+        height: printTemplate.pageSizeY + 'mm',
+        padding: page.marginTop + 'mm ' + page.marginRight + 'mm ' + page.marginBottom + 'mm ' + page.marginLeft + 'mm'
     });
-    $("#" + field.name + " .value").css({
-      top: field.valueTop + page.marginTop,
-      left: field.valueLeft + page.marginLeft
+
+    console.log(page.marginTop + 'mm ' + page.marginRight + 'mm ' + page.marginBottom + 'mm ' + page.marginLeft + 'mm');
+
+    // fields
+    $.each(this.data.item.printTemplate.fields, function(index, field) {
+
+
+        // line items
+        if (field.name == "lineItems_table") {
+            $("table#" + field.name).css({
+                top: field.labelTop + page.marginTop + 'mm',
+                left: field.labelLeft + page.marginLeft + 'mm'
+            });
+        }
+
+        var lineItemFields = [
+            'lineItems_number',
+            'lineItems_name',
+            'lineItems_unit',
+            'lineItems_unitPrice',
+            'lineItems_quantity',
+            'lineItems_totalPrice'
+        ];
+
+        var totalFields = [
+            'total_beforeGST',
+            'total_GST',
+            'total_afterGST'
+        ];
+
+        if (_.contains(lineItemFields, field.name)) {
+            // line items fields
+
+            $("#" + field.name).css({
+                width: field.valueWidth + 'mm'
+            });
+
+        } else {
+            if (_.contains(totalFields, field.name)) {
+                // total fields
+
+                $("#" + field.name + " .key").css({
+                    top: field.labelTop + page.marginTop + 'mm',
+                    right: field.labelRight + page.marginRight + 'mm',
+                    'text-align': 'right'
+                });
+                $("#" + field.name + " .value").css({
+                    top: field.valueTop + page.marginTop + 'mm',
+                    right: field.valueRight + page.marginRight + 'mm',
+                    'text-align': 'right'
+                });
+
+            } else {
+                // normal fields
+
+                $("#" + field.name + " .key").css({
+                    top: field.labelTop + page.marginTop + 'mm',
+                    left: field.labelLeft + page.marginLeft + 'mm'
+                });
+                $("#" + field.name + " .value").css({
+                    top: field.valueTop + page.marginTop + 'mm',
+                    left: field.valueLeft + page.marginLeft + 'mm'
+                });
+            }
+        }
     });
-  })
+
 });
